eb54198bfa9945edf59fdad737dfef4cffa9563a
Christopher Walle <cjwalle97@gmail.com>
2018-02-28 17:00:31 -0600

    Update
    Update

---
+++ GeneticAlgorithm.py
@@ -0,0 +1,8 @@
+class Result (object):
+    '''Desired end result of Gentic Algorithm'''
+    def __init__(self, goal):
+        self.goal = goal
+        for i in goal
+        self.letters += 1
+        
+        
