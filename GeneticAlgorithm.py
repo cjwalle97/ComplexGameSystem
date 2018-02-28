58a9ccd060066a8db1cb7a48459d3d3d568b7464
Christopher Walle <cjwalle97@gmail.com>
2018-02-28 17:35:12 -0600

    Update
    Update

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -1,8 +1,20 @@
-class Result (object):
+class Goal (object):
     '''Desired end result of Gentic Algorithm'''
     def __init__(self, goal):
         self.goal = goal
-        for i in goal
-        self.letters += 1
-        
-        
+        letterlist = []
+        for i in goal:
+            self.letters += 1
+            letterlist.append[i]
+
+class Solution (object):
+    '''list of characters that must match up to Goal's LetterList'''
+    def __init__(self)
+        list1 = 
+
+def CheckSolution(solution, goal):
+    '''Checks the current solution to see if it matches the goal'''
+    if solution == goal:
+        return true
+
+
